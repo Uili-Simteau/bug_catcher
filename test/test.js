7ecd8a4d2172a5889f84042c094a4d93981d0319
Uili Fecteau <email": null>
2016-06-08 14:59:53 +1200

    added checkbox to list

--- test/test.js
+++ test/test.js
@@ -1,1 +1,11 @@
-test.js
+import Reeact from 'react'
+import { mount, shalow } from 'enzyme'
+
+describe('', ( => {
+
+	it('calls componentDidMount', () => {
+		const wrapper = mount (<Foo />);
+		expect(Foo.portotype.componentDidMount.calledOnce).to.equal(true);
+	})
+}));
+
