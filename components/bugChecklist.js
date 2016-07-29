8ded922a48f8add5a8ea976e990ccc68e0b58e1d
Uili-Simteau <simteau@gmail.com>
2016-07-29 15:54:40 +1200

    fix indentation, add normalise to head

--- components/bugChecklist.js
+++ components/bugChecklist.js
@@ -4,30 +4,29 @@ import reactDom from 'react-dom'
 import Bug from './bug.js'
 
 class BugChecklist extends Component{
-	constructor(props) {
-		super(props)
+    constructor(props) {
+        super(props)
+        console.log("bugs BugChecklist got from App", this.props.bugs)
+      },
 
-		console.log("bugs BugChecklist got from App", this.props.bugs)
-	}
-
-	render () {
-		const bugs = this.props.bugs
-
-		return (
-			<div>
-				<ul>
-					{ bugs.map( (singleBug, i) => { 
-						  return (
-						  	<div>
-							  	<input  id="checkbox" value="#"/><Bug class="singleBug" bug={singleBug} index={ i } />
-							  	<input id='solution' ref='solution' type="text" placeholder="Solution?"></input>
-							  </div> 
-					  	)} 
-				  )}
-				</ul>
-			</div>
-			)
-	}
+    render () {
+        const bugs = this.props.bugs
+        return (
+            <div>
+                <ul>
+                    { bugs.map( (singleBug, i) => {
+                      return (
+                          <div>
+                              <input  id="checkbox" value="#"/><Bug class="singleBug" bug={singleBug} index={ i } />
+                              <input id='solution' ref='solution' type="text" placeholder="Solution?"></input>
+                              <button id="solutionButton" type="submit" onClick={this.submitSolution.bind(this)}>Submit solution</button>
+                          </div>
+                      )
+                    })}
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default BugChecklist
