55d384b62b78c0c35f0103f0fb42e9e336492add
Uili-Simteau <simteau@gmail.com>
2016-08-03 15:20:16 +1200

    comma errors in pulling together bundle.js

--- components/bugChecklist.js
+++ components/bugChecklist.js
@@ -1,4 +1,4 @@
-import React { Component } from 'react'
+import React, { Component } from 'react'
 import reactDom from 'react-dom'
 
 import Bug from './bug.js'
@@ -25,11 +25,6 @@ class BugChecklist extends Component{
                     { bugs.map( (singleBug, i) => {
                         return (
                             <div>
-                            <!-- LikeBtn.com BEGIN -->
-                                <span class="likebtn-wrapper" data-theme="google" data-i18n_like="caught" data-identifier="item_1" data-dislike_enabled="false" data-icon_dislike_show="false" data-voting_frequency="2" data-counter_clickable="true" data-counter_zero_show="true" data-popup_hide_on_outside_click="false">
-                                </span>
-                                <script>(function(d,e,s){if(d.getElementById("likebtn_wjs"))return;a=d.createElement(e);m=d.getElementsByTagName(e)[0];a.async=1;a.id="likebtn_wjs";a.src=s;m.parentNode.insertBefore(a, m)})(document,"script","//w.likebtn.com/js/w/widget.js");</script>
-                            <!-- LikeBtn.com END -->
                                 <input id="solution" ref="solution" type="text" placeholder="No Solution?"></input>
                                 <button id="solutionButton" type="submit" onClick={this.submitSolution.bind(this)}>Submit solution</button>
                             </div>
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-import React { Component } from 'react'
 import reactDom from 'react-dom'
+import React, { Component } from 'react'
 
 import Title from './components/title.js'
 import BugForm from './components/bugForm.js'
@@ -10,45 +10,41 @@ import Bug from './components/bug.js'
 
 
 class App extends Component {
-    constructor(props){
-        super(props)
-        this.addBug = this.addBug.bind(this)
-        this.addSolution = this.addSolution.bind(this)
-        this.state = {
-            bugs: ["comma", "bracket"],
-            solutions: []
-        }
-    },
-
-    addBug (bug) {
-        console.log("this is the bug", bug)
-        console.log("here's what I think this is:", this)
-
-        const caughtBugs = this.state.bugs
-
-        console.log("here's the new bugs state", [...caughtBugs, bug])
-
-        this.setState({
-            bugs: [...caughtBugs, bug]
-        })
-        console.log('caught bugs!', this.state.bugs)
-    },
-
-    addSolution (solution) {
-        console.log("this is the solution", solution)
-        console.log("here's what I think this is:", this)
-
-        const caughtSolutions = this.state.solutions
-
-        console.log("here's the new bugs state", [...caughtSolutions, solution])
-
-        this.setState({
-            solutions: [...caughtSolutions, solution]
-        })
-        console.log('caught bugs!', this.state.solution)
-    },
-
-
+    // constructor(props) {
+    //     super(props)
+    //     this.addBug = this.addBug.bind(this)
+    //     this.addSolution = this.addSolution.bind(this)
+    //     this.state = {
+    //         bugs: ["comma", "bracket"],
+    //         solutions: []
+    //     }
+    // },
+    // addBug (bug) {
+    //     console.log("this is the bug", bug)
+    //     console.log("here's what I think this is:", this)
+    //
+    //     const caughtBugs = this.state.bugs
+    //
+    //     console.log("here's the new bugs state", [...caughtBugs, bug])
+    //
+    //     this.setState({
+    //         bugs: [...caughtBugs, bug]
+    //     })
+    //     console.log('caught bugs!', this.state.bugs)
+    // },
+    // addSolution (solution) {
+    //     console.log("this is the solution", solution)
+    //     console.log("here's what I think this is:", this)
+    //
+    //     const caughtSolutions = this.state.solutions
+    //
+    //     console.log("here's the new bugs state", [...caughtSolutions, solution])
+    //
+    //     this.setState({
+    //         solutions: [...caughtSolutions, solution]
+    //     })
+    //     console.log('caught bugs!', this.state.solution)
+    // },
     render() {
         return (
             <div>
